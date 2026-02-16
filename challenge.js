9a921dcdf0eb5dcaca5f64eefcb2ee4c194c902b
charlotte-manzi <429461@etud.uco.fr>
2026-02-16 18:16:08 +0100

    feat(complete): ajout de la commande complete au routing

--- challenge.js
+++ challenge.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 const { genererDefi } = require('./commands/generate');
+const { completerDefi } = require('./commands/complete');
+const { calculerSerie } = require('./commands/streak');
 
 // Récupérer la commande
 const commande = process.argv[2];
@@ -11,9 +13,19 @@ switch(commande) {
     genererDefi();
     break;
   
+  case 'complete':
+    completerDefi();
+    break;
+  
+  case 'streak':
+    calculerSerie();
+    break;
+  
   default:
     console.log('\n🏋️  Fitness Challenge CLI 🏋️\n');
     console.log('Commandes disponibles:');
-    console.log('  generate  - Générer un nouveau défi\n');
+    console.log('  generate  - Générer un nouveau défi');
+    console.log('  complete  - Marquer le défi comme complété');
+    console.log('  streak    - Voir ta série actuelle\n');
     break;
 }
